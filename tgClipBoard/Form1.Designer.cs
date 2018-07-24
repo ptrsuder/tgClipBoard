f5e8c89bd30e9308ef8e4519fbc6e44b26d985b4
ptrsuder <ptrsuder@users.noreply.github.com>
2018-07-24 18:32:27 +0300

    Add files via upload

---
+++ tgClipBoard/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace tgClipBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new tgClipBoard.ClipboardMonitor();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(28, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.Visible = false;
+            this.button1.ClipboardChanged += new System.EventHandler<tgClipBoard.ClipboardChangedEventArgs>(this.button1_ClipboardChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "/tg/ clipBoarder";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 125);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClipboardMonitor button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ tgClipBoard/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tgClipBoard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Minimized;                        
+        }
+
+        private void button1_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            try
+            {
+                string g;
+                //string clipboardContentType = e.DataObject.GetType().ToString(); ;
+                //if (clipboardContentType == DataFormats.Text)
+                g = e.DataObject.GetData(DataFormats.Text).ToString();
+                string patternVola, patternMega, patternSendspace, patternMediafire;
+                patternVola = "^[/]?r/.+";
+                patternMega = "^[/]?#F!.+";
+                patternSendspace = "^[/]?file.{6}|[/]?filegroup/.{6}";
+                //patternSendspaceGroup = "[/]?file[group]?.{6}";
+                patternMediafire = "^[/]?(?:file|folder)/.{10,}";
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add(patternVola, "volafile.org");
+                dic.Add(patternMega, "mega.nz");
+                dic.Add(patternSendspace, "sendspace.com");
+                dic.Add(patternMediafire, "mediafire.com");
+                List<string> patterns = new List<string> { patternVola, patternSendspace, patternMega, patternMediafire };
+                foreach(string pattern in dic.Keys)
+                {
+                    if (Regex.Match(g, pattern).Success)
+                    {
+                        if (!g.StartsWith("/"))
+                            g = "/" + g;
+                        System.Diagnostics.Process.Start("http://" + dic[pattern] + g);
+                        //notifyIcon1.BalloonTipText = dic[pattern];
+                        //notifyIcon1.ShowBalloonTip(1);
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Swallow or pop-up, not sure
+                // Trace.Write(e.ToString());
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+        
